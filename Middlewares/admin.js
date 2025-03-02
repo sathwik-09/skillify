7a7bbaef2d07f970eded61caa536902518b5a507
Sathwik <enapothulasathwik09@gmail.com>
2025-03-02 14:44:02 +0530

    feat: add user middleware

---
+++ Middlewares/admin.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const adminMiddleware = (req,res,next)=>{
+  const token = req.headers.token;
+  const decodedData = jwt.verify(token, process.env.JWT_ADMIN_SECRET)
+  if(decodedData){
+    req.adminId = decodedData.id;
+    next();
+  }else{
+    res.status(403).json({
+      message: "You need to sign in"
+    })
+  }
+}
+
+module.exports = {
+  userMiddleware: adminMiddleware
+}
