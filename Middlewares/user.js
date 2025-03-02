e6566e8cc94f0cde35c3903f3d035bc00944c287
Sathwik <enapothulasathwik09@gmail.com>
2025-03-02 14:44:44 +0530

    feat: add user middleware

---
+++ Middlewares/user.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const userMiddleware = (req,res,next)=>{
+  const token = req.headers.token;
+  const decodedData = jwt.verify(token, process.env.JWT_USER_SECRET)
+  if(decodedData){
+    req.userId = decodedData.id;
+    next();
+  }else{
+    res.status(403).json({
+      message: "You need to sign in"
+    })
+  }
+}
+
+module.exports = {
+  userMiddleware: userMiddleware
+}
